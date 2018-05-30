e2dcc752c2255417595f56d255ca0be9788519b8
Nathan Park <nyp5aa@virginia.edu>
2018-05-30 16:09:41 -0400

    work

--- src/Results.js
+++ src/Results.js
@@ -5,22 +5,22 @@ import "./Results.css";
 let list = [];
 
 class Results extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { candidate } = this.props;
-    const { office, name, house } = candidate;
-    return (
-      <div className="Footer2">
-        <div className="Background2" id="border">
-          <div className="officeText">{office}</div>
-          <div className="nameText">{name}</div>
-          <div className="partyText">{house}</div>
-        </div>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const { candidate } = this.props;
+        const { office, name, party, candidateUrl, email, gotFacebook, gotTwitter } = candidate;
+        return (
+            <div className="Footer2">
+                <div className="Background2" id="border">
+                    <div className="officeText">{office}</div>
+                    <div className="nameText">{name}</div>
+                    <div className="partyText">{party}</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Results;
