84ad35fb649fba0f03b7ae2c10cf05c484e16259
Edward Radion <er2cg@virginia.edu>
2018-05-31 11:11:03 -0400

    formated link to registration

--- src/components/registertext.js
+++ src/components/registertext.js
@@ -3,29 +3,28 @@ import "./registertext.css";
 import { ButtonToolbar, Button } from "react-bootstrap";
 
 class RegisterText extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        const { dataForHeader } = this.props;
-        return (
-            <div className="headerForResults">
-                Are you registerd to vote in {" "}
-                {dataForHeader[0]},{" "}
-                {dataForHeader[1]}? If not,{" "}
-                <ButtonToolbar>
-                    <Button
-                        id="registerHereAddress"
-                        bsStyle="success"
-                        bsSize="large"
-                        href={dataForHeader[2]}
-                    >
-                        REGISTER HERE
-                    </Button>
-                </ButtonToolbar>
-            </div >
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { dataForHeader } = this.props;
+    return (
+      <div className="headerForResults">
+        Are you registerd to vote in {dataForHeader[0]}, {dataForHeader[1]}?{" "}
+        <ButtonToolbar>
+          <Button
+            id="registerHereAddress"
+            bsStyle="success"
+            bsSize="large"
+            href={dataForHeader[2]}
+            target="_blank"
+          >
+            REGISTER HERE
+          </Button>
+        </ButtonToolbar>
+      </div>
+    );
+  }
 }
 
 export default RegisterText;
