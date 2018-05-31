d674146aa26ad93c7ef3634742af51799e4da0ee
Edward Radion <er2cg@virginia.edu>
2018-05-31 10:11:11 -0400

    added party affiliation colors

--- src/Results.js
+++ src/Results.js
@@ -8,6 +8,22 @@ class Results extends Component {
   constructor(props) {
     super(props);
   }
+
+  // making political affiliation have colors
+  colorizing_parties(party) {
+    let affiliation = "gray";
+    if (party.includes("Democratic")) {
+      affiliation = "blue";
+    } else if (party.includes("Republican")) {
+      affiliation = "red";
+    } else if (party.includes("Green")) {
+      affiliation = "green";
+    } else if (party.includes("Libertarian")) {
+      affiliation = "yellow";
+    }
+    return affiliation;
+  }
+
   render() {
     const { candidate } = this.props;
     const {
@@ -20,24 +36,14 @@ class Results extends Component {
       gotTwitter
     } = candidate;
 
-    // making political affiliation have colors
-    let affiliation = "gray";
-    if(party === "Democratic"){
-      affiliation = "blue"
-    }
-    else if(party === "Republican") {
-      affiliation = "red"
-    }
-    else if(party === "Green") {
-      affiliation = "green"
-    }
+    let affiliation_color = this.colorizing_parties(party);
 
     return (
       <div className="spacer">
         <div className="textbox" id="border">
           <div className="officeText">{office}</div>
           <div className="nameText">{name}</div>
-          <div className={affiliation}>{party}</div>
+          <div className={affiliation_color}>{party}</div>
           <div className="websiteText">{candidateUrl}</div>
           <div className="emailText">{email}</div>
           <div className="facebookText">{gotFacebook}</div>
