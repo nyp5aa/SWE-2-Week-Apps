12c9cfea9e194930436d1156e9d30a13b3fe4c72
Nathan Park <nyp5aa@virginia.edu>
2018-05-31 22:49:55 -0400

    hopefully i dont delete anything

--- src/App.js
+++ src/App.js
@@ -5,30 +5,94 @@ import Button from "./components/button";
 import Results from "./Results";
 import axios from "axios";
 import logo from "./informlogo.png";
+import RegisterText from "./components/registertext";
+import RegionalText from "./components/regionaltext";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       dataHere: false,
-      allData: null
+      allData: [],
+      headerData: [],
+      footerData: []
     };
   }
-  changeDataHere=(dummyList)=>{
+  changeDataHere = (dummyList, forHeader, forFooter) => {
     this.setState({
       dataHere: true,
-      allData: dummyList
+      allData: dummyList,
+      headerData: forHeader,
+      footerData: forFooter
     });
   };
+  splitSenateAndHouse(candidates) {
+    let senateEndIndex = 0;
+    for (let i = 0; i < this.state.allData.length; i++) {
+      if (this.state.allData[i].office.includes("Senat")) {
+        senateEndIndex++;
+      }
+    }
+    let returnThis = [
+      candidates.slice(0, senateEndIndex),
+      candidates.slice(senateEndIndex, candidates.length)
+    ];
+    return returnThis;
+  }
+  getProperName(congress) {
+    let returnSenate = "";
+    let returnHouse = "";
+    for (let i = 0; i < this.state.allData.length; i++) {
+      if (this.state.allData[i].office.includes("Senat")) {
+        returnSenate = this.state.allData[i].office
+      }
+      else {
+        returnHouse = this.state.allData[i].office
+      }
+    }
+    if (congress === "senate") {
+      return returnSenate;
+    }
+    else {
+      return returnHouse;
+    }
+  }
   render() {
-    {console.log(this.state.dataHere)}
-    if(this.state.dataHere){
-      const candidates = this.state.allData.map(candidate => {
-        return <Results candidate={candidate} />
+    if (this.state.dataHere) {
+      const candidates = this.state.allData.map(person => {
+        return <Results candidate={person} />;
       });
-      <div>
-        hello world!
-      </div>
+      let congress = this.splitSenateAndHouse(candidates);
+      let properSenateName = this.getProperName("senate");
+      let properHouseName = this.getProperName("house");
+      return (
+        <div>
+          <RegisterText dataForHeader={this.state.headerData} />
+          {congress[0].length != 0 && (
+            <div>
+              <div className="officeText">
+                Senate: {properSenateName} election
+              </div>
+              <div className="senate">
+                {congress[0]}
+              </div>
+            </div>
+          )}
+          <div className="spacer" />
+          {congress[1].length != 0 && (
+            <div>
+              <div className="officeText">
+                House of Representative: {properHouseName} election
+                </div>
+              <div className="house">
+                {congress[1]}
+              </div>
+            </div>
+          )}
+          <div className="spacer" />
+          <RegionalText dataForFooter={this.state.footerData} />
+        </div>
+      );
     }
     return (
       <div>
@@ -39,13 +103,12 @@ class App extends Component {
           </header>
           <h1 id="bborder">Enter in your address to find local elections</h1>
           <div className="bigdiv">
-            <div className="search">{<Searchbar />}</div>
-              <div className="poorlynamedvariable">
-                <Button changeDataHere={this.changeDataHere}/>
-              </div>
+            <div className="search">{<Searchbar changeDataHere={this.changeDataHere} />}</div>
+            <div className="poorlynamedvariable">
+              <Button changeDataHere={this.changeDataHere} />
             </div>
           </div>
-        <div className="Footer" />
+        </div>
       </div>
     );
   }
--- src/Results.js
+++ src/Results.js
@@ -1,30 +1,69 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "./Results.css";
+import facebooklogo from "./facebooklogo.png";
+import twitterlogo from "./twitterlogo.png";
 
-let list = [];
+class Results extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-class Results extends Component{
-    constructor(props){
-        super(props);
+  // making political affiliation have colors
+  colorizing_parties(party) {
+    let affiliation = "gray";
+    if (party.includes("Democra")) {
+      affiliation = "blue";
+    } else if (party.includes("Republ")) {
+      affiliation = "red";
+    } else if (party.includes("Green")) {
+      affiliation = "green";
+    } else if (party.includes("Libert")) {
+      affiliation = "yellow";
     }
-    render(){
-        const { candidate } = this.props;
-        const { office, name, house } = candidate;
-        {console.log("hello world")}
-        return(
-            <div>
-                <div>
-                    { name }
-                </div>
-                <div>
-                    { house }
-                </div>
-                <div>
-                    { office }
-                </div>
-            </div>
-        );
-    }
+    return affiliation;
+  }
+
+  render() {
+    const { candidate } = this.props;
+    const { name, party, candidateUrl, email } = candidate;
+
+    let affiliation_color = this.colorizing_parties(party);
+
+    return (
+      <div className="individiual-candidate-display">
+
+        {candidate.facebook && (
+          <a href={candidate.facebook} target="_blank">
+            <img
+              src={facebooklogo}
+              className="facebook-logo"
+              alt="facebook profile"
+            />{" "}
+          </a>
+        )}
+        {candidate.twitter && (
+          <a href={candidate.twitter} target="_blank">
+            <img
+              src={twitterlogo}
+              className="twitter-logo"
+              alt="twitter profile"
+            />{" "}
+          </a>
+        )}
+
+        <div className="nameText">{name}</div>
+
+        <div className={affiliation_color}>{party}</div>
+        <div className="emailText">{email}</div>
+        <a href={candidateUrl} target="_blank">
+          <div className="websiteText" href={candidateUrl} target="_blank">
+            {candidateUrl}
+          </div>
+        </a>
+      </div>
+    );
+  }
 }
 
-export default Results
+export default Results;
